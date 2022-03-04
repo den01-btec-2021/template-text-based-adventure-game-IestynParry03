7eb92d6209787cba629f328175912a9a58ab0d00
Iestyn Parry <29750@bridgend.ac.uk>
2022-03-04 13:13:55 +0000

    added live counter

--- game.py
+++ game.py
@@ -1,5 +1,12 @@
 def main():
     print("Welcome to my game!")
     Player_name = input("What is your name?")
-    print("Hi" + Player_name)
+    print(" Hi " + Player_name)
+    Lives = 3
+    print(f" you have {Lives} remaining ")
+
+
+
+
+
 main()
