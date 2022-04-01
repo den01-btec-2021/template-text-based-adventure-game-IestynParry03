690daa7ab8cc6800b6cbb6800c0f176a6909b06d
Iestyn Parry <29750@bridgend.ac.uk>
2022-04-01 10:28:27 +0100

    new changes

--- game.py
+++ game.py
@@ -1,116 +1,70 @@
-from audioop import add
-from msilib.schema import Directory
-
-
 def main():
-
-    print("Welcome to my game!")
-    Player_name = input("What is your name?")
-    print(" Hi " + Player_name)
-    lives = 3
     
-    print(f" you have {lives} lives remaining ")
+    print("Welcome to the Game")
 
-    backpack = [] #intitalise empty list for backpack
-    print("there are 4 directions to choose from North, East, South, West")
+    player_name = input("what is your name? ")
+
+    print(f"Hello {player_name}.")
+
+    lives = 3
+    print(f"You have {lives} remaining lives.")
+
+    backpack = [] # initialise empty list for backpack
 
     while True:
+      direction = input("Which direction do you want to go? Please choose from North, South, East or West.")    
 
-        player_direction = input("Which direction do you want to go?")
+      if direction == "North":
+        room_direction = "North"
+        puzzle = "What is 2 + 2? "
+        puzzle_solution = "4"
+        key_number = "1"
+        lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
 
-        if player_direction == "North":
-            room_direction = "North"
-            puzzle = "What is 7+7? "
-            puzzle_solution = "14"
-            key_number = "1"
-            lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
-            
-        
-        elif player_direction == "South":
-            room_direction = "South "
-            puzzle = "What is 5+5?"
-            puzzle_solution = "10"
-            key_number = "2"
-            lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
-            
-            
-        
-        elif player_direction == "East":
-            room_direction = "East"
-            puzzle = "What is 9+9? "
-            puzzle_solution = "18"
-            key_number = "3"
-            lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
-            
-                
+      elif direction == "South":
+        room_direction = "South"
+        puzzle = "What is 2 + 3? "
+        puzzle_solution = "5"
+        key_number = "2"
+        lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
 
-                 
-        elif player_direction == "West":
-            room_direction = "West"
-            puzzle = "What is 6+6? "
-            puzzle_solution = "12"
-            key_number = "4"
-            lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
-            
-                  
-        
-        else:
-            print("invalid response")
+      elif direction == "East":
+        room_direction = "East"
+        puzzle = "What is 2 + 4? "
+        puzzle_solution = "6"
+        key_number = "3"
+        lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
+      
+      elif direction == "West": 
+        room_direction = "West"
+        puzzle = "What is 2 + 5? "
+        puzzle_solution = "7"
+        key_number = "4"
+        lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
 
-        
-        # if backpack is full, open door, win game
-        if ("Key 1" in backpack) and ("Key 2" in backpack) and ("Key 3" in backpack) and ("Key 4" in backpack):
-            print("Congrations you have collected all keys and won the game!")
-            exit()
-        
-        if lives ==0:
-            print("Sorry, you died")
-            exit()
+      else:
+        print("Room not recognised!")   
+
+      #if back pack is full, open door and win game.
+      if ("Key 1" in backpack) and ("Key 2" in backpack) and ("Key 3" in backpack) and ("Key 4" in backpack):
+        print("Win!") 
+        exit()
+
+      if lives == 0:
+        print("Lose!")
+        exit()
 
 def in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number):
-    print(f"You entered the {room_direction} Room.")
-    puzzle_guess = input(puzzle)
-    if puzzle_guess == puzzle_solution:
-        print(f"Correct. Key {key_number} collected.")
-        if f"Key {key_number}" not in backpack:
-            backpack.append(f"Key {key_number}")
-        else:
-            print("You have already collected this Key!")
-    else:
-        lives -= 1
-        print(f"you have lost a life, you now have {lives} remaining ")
-        
-    return lives
+  print(f"You entered the {room_direction} Room.")
+  puzzle_guess = input(puzzle)
+  if puzzle_guess == puzzle_solution:
+    print(f"Correct. Key {key_number} collected.")
+    backpack.append(f"Key {key_number}")
+  else:
+    lives -= 1
+    print(f"Incorrect. You have {lives} lives remaining.")
 
-def addition(a,b):
-    return a+b
+  return lives
 
-def test_addition():
-    assert addition(3,5) == 8
-    assert addition(-1,0) == -1
-    assert addition(-1,1) == 0
-
-def test_in_room():
-
-    backpack = []
-    lives = 3
-    room_direction = "North"
-    puzzle = "What is 6 + 6?"
-    puzzle_solution = "12"
-    key_number = "1"
-
-    #check correct??
-    assert in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number) == 2 # lives -1
-    assert f"Key {key_number}" in backpack #check that this key is the backpack is in the backpack
-    
-    
-    in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number) == 3
-    assert backpack != ["Key 1","Key 1"]
-    
-    
-    if __name__ == "__main__":
-        
-        #main()
-        #value = addition(3,2)
-        #print(value)
-        test_addition()
+if __name__ == '__main__':
+  main()
